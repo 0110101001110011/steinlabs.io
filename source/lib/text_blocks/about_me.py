c17abe90cb9fc010839809015f550a56e9193a48
James Stanton <jestanton@outlook.com>
2018-09-25 01:52:06 +0900

    Updated css and tweaks a vew bits of html for navbar. Also fixed some text.

--- source/lib/text_blocks/about_me.py
+++ source/lib/text_blocks/about_me.py
@@ -5,12 +5,12 @@ Text block for the "About Me" Section of the site
 TEXT_JP = '''
 <hr></hr>
 <h3><span style="color: {highlight_color}">初めまして</span></h3>
-> <b>名前</b>: スタントン ジェームズ 瑛之助と申します。<br/>
-> <b>職業</b>: 英国キングストン大学でゲームテクノロジーを専攻している二年生です。<br/>
-> <b>現在の状況</b>: ソニーインタラクティブエンタテインメントの東京本社で一年間のインターンシップを努めています。<br/>
-> <b>興味があるところ</b>: ゲーム開発、特にゲームプレイプログラミングとエンジンプログラミングには興味があります。<br/>
-> <b>趣味</b>: ビデオゲーム、アニメ、プログラミング、学び、仲間と時間を過ごす事、食べ物が趣味です。<br/>
-> <b>現在地</b>: 中央東京。<br/>
+• <b>名前</b>: スタントン ジェームズ 瑛之助と申します。<br/>
+• <b>職業</b>: 英国キングストン大学でゲームテクノロジーを専攻している二年生です。<br/>
+• <b>現在の状況</b>: ソニーインタラクティブエンタテインメントの東京本社で一年間のインターンシップを努めています。<br/>
+• <b>興味があるところ</b>: ゲーム開発、特にゲームプレイプログラミングとエンジンプログラミングには興味があります。<br/>
+• <b>趣味</b>: ビデオゲーム、アニメ、プログラミング、学び、仲間と時間を過ごす事、食べ物が趣味です。<br/>
+• <b>現在地</b>: 中央東京。<br/>
 <br/>
 夏2020年に卒業するつもりです。<br/>
 卒業後、日本へ引っ越し日本でゲーム開発者の仕事を求職します。<br/>
@@ -18,30 +18,30 @@ TEXT_JP = '''
 よろしくお願いします！<br/>
 <hr></hr>
 <h3><span style="color: {highlight_color}">スキル</span></h3>
-> <b>プログラミング言語</b>: C++，C# .NET、Python、Bash、ブループリント。<br/>
-> <b>ゲームエンジン</b>: Unreal、Unity。<br/>
-> <b>ツールや技術</b>: Maya、Adobe Premiere CC、Git/Github、OpenGL。<br/>
-> <b>その他</b>: エンタープライズ情報技術、Enterprise IT, バックエンドソフトウェア開発、人間関係。<br/>
+• <b>プログラミング言語</b>: C++，C# .NET、Python、Bash、ブループリント。<br/>
+• <b>ゲームエンジン</b>: Unreal、Unity。<br/>
+• <b>ツールや技術</b>: Maya、Adobe Premiere CC、Git/Github、OpenGL。<br/>
+• <b>その他</b>: 日本語（約JLPT2評価）、エンタープライズ情報技術、Enterprise IT, バックエンドソフトウェア開発、人間関係。<br/>
 '''
 
 TEXT_EN = '''
 <hr></hr>
 <h3><span style="color: {highlight_color}">Introduction</span></h3>
-> <b>My name is</b>: James Einosuke Stanton.<br/>
-> <b>I am</b>: A University student, studying Games Technology at Kingston University, England (second year completed in summer 2018).<br/>
-> <b>I am currently</b>: Doing a 1 year internship at Sony Interactive Entertainment in Tokyo, Japan.<br/>
-> <b>I am interested in</b>: Game development, specifically gameplay programming and engine programming.<br/>
-> <b>My hobbies include</b>: Video games, anime, programming, learning, going out with my friends, and food.<br/>
-> <b>I currently live</b>: In central Tokyo.<br/>
+• <b>My name is</b>: James Einosuke Stanton.<br/>
+• <b>I am</b>: A University student, studying Games Technology at Kingston University, England (second year completed in summer 2018).<br/>
+• <b>I am currently</b>: Doing a 1 year internship at Sony Interactive Entertainment in Tokyo, Japan.<br/>
+• <b>I am interested in</b>: Game development, specifically gameplay programming and engine programming.<br/>
+• <b>My hobbies include</b>: Video games, anime, programming, learning, going out with my friends, and food.<br/>
+• <b>I currently live</b>: In central Tokyo.<br/>
 <br/>
 I will graduate university around the spring/summer of 2020.<br/>
 After graduation, I will relocate to Japan permanently and seek employment as a game developer.<br/>
 <br/>
 Thank you for visiting my portfolio site!<br/>
-<hr></hr>
+<br/>
 <h3><span style="color: {highlight_color}">Key Skills</span></h3>
-> <b>Programming languages</b>: C++, C# .NET, Python, Bash, Blueprints.<br/>
-> <b>Game Engines</b>: Unreal Engine 4, Unity.<br/>
-> <b>Tools and technologies</b>: Maya, Adobe Premiere CC, Git/Github, OpenGL.<br/>
-> <b>Misc</b>: Enterprise IT, Back-end software engineering, Customer relations and interactions.<br/>
+• <b>Programming languages</b>: C++, C# .NET, Python, Bash, Blueprints.<br/>
+• <b>Game Engines</b>: Unreal Engine 4, Unity.<br/>
+• <b>Tools and technologies</b>: Maya, Adobe Premiere CC, Git/Github, OpenGL.<br/>
+• <b>Misc</b>: Japanese Language(Professional working proficiency), Enterprise IT, Back-end software engineering, Customer relations and interactions.<br/>
 '''
