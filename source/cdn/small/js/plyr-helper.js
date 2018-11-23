54d6b8d3e0df0f57d6d0891c0c2e0eeeebbfa5fd
James Stanton <jestanton@outlook.com>
2018-11-23 23:29:23 +0900

    Minified all the things

--- source/cdn/small/js/plyr-helper.js
+++ source/cdn/small/js/plyr-helper.js
@@ -1,6 +1,8 @@
-const VIDEO_PLAYERS = document.getElementsByClassName("vid");
-for (let index = 0; index < VIDEO_PLAYERS.length; index++) {
-    this.vps = [];
-    const player = new Plyr("#" + VIDEO_PLAYERS[index].id, {});
-    this.vps[index] = player;
+"use strict";
+
+var VIDEO_PLAYERS = document.getElementsByClassName("vid");
+var vps = [];
+for (var index = 0; index < VIDEO_PLAYERS.length; index++) {
+    var player = new Plyr("#" + VIDEO_PLAYERS[index].id, {});
+    vps[index] = player;
 }
