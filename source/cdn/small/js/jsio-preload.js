201e20a4ca23641d657cf7d73fa313b6982c010c
james_stanton <james.stanton@sony.com>
2019-01-25 16:28:55 +0900

    Work page and scroll blocking is working ish

--- source/cdn/small/js/jsio-preload.js
+++ source/cdn/small/js/jsio-preload.js
@@ -16,7 +16,9 @@
         COL_GREY: Color(78, 78, 78),
         COL_WHITE: Color(255, 255, 255),
         NUM_LOADINGBAR_WIDTH_MAX: 100,
-        DELAY_FADEOUTS: [500, 1000, 1000]
+        DELAY_FADEOUTS: [500, 1000, 1000],
+        SCROLL_BLOCK_KEYCODES: [32, 33, 34, 34, 35, 36, 38, 40],
+        SCROLLBAR_WIDTH: "-" + (window.innerWidth - document.documentElement.clientWidth) + "px"
     }
 
     const STR = {
@@ -24,6 +26,9 @@
         CCLASS_FADE_OUT_LOADINGOVERLAY: "h-fade-out-easein-1s",
         CCLASS_HIDDEN: "h-hidden",
         CCLASS_WORKVIDEO_WRAPPER: "work-vid-wrapper",
+        CCLASS_WORKPAGE_EXITBUTTON: "work-page-xbutton",
+        CCLASS_WORKPAGE_HIDDEN: "work-page-hidden",
+        CCLASS_WORKPAGE_TILE: "work-item-wrapper",
         FLEXEND: "flex-end",
         LOADINGBAR_TRANSITION_POSTLOAD: "width 1s ease-in, visibility 1s, opacity 1s ease-in",
         ALLTRANS_INSTANT: "all 0s",
@@ -42,7 +47,6 @@
         let blue = original.b + ((target.b - original.b) * delta);
 
         let alpha = null;
-
 
         if (original.a && !target.a) { // If original has alpha, but not target, assume target alpha is 1
             alpha = original.a + ((1 - original.a) * delta);
@@ -69,6 +73,29 @@
 
     function lerpNum(original, target, delta) {
         return (!isNaN(original) && !isNaN(target) && !isNaN(delta)) ? (original + ((target - original) * delta)) : 0;
+    }
+
+    function consumeEvent(e) {
+        if (e.type === "keydown" && !VAL.SCROLL_BLOCK_KEYCODES.includes(e.which)) {
+            return;
+        } 
+
+        e.stopPropagation();
+        e.preventDefault();
+    }
+
+    function toggleScrolling(on) {
+        if (on) {
+            document.documentElement.removeEventListener("keydown", consumeEvent, {passive: false}); 
+            document.documentElement.removeEventListener("mousedown", consumeEvent, {passive: false}); 
+            document.documentElement.removeEventListener("touchmove", consumeEvent, {passive: false});  
+            document.documentElement.removeEventListener("wheel", consumeEvent, {passive: false});
+        } else {
+            document.documentElement.addEventListener("keydown", consumeEvent, {passive: false}); 
+            document.documentElement.addEventListener("mousedown", consumeEvent, {passive: false}, true); 
+            document.documentElement.addEventListener("touchmove", consumeEvent, {passive: false});   
+            document.documentElement.addEventListener("wheel", consumeEvent, {passive: false});  
+        }
     }
 
     document.addEventListener("DOMContentLoaded", function () {
@@ -121,6 +148,55 @@
                 }
             });
 
+            vid.addEventListener("play", function (e) {
+                playbutton.classList.add(STR.CCLASS_HIDDEN);
+            });
+
+            vid.addEventListener("pause", function (e) {
+                playbutton.classList.remove(STR.CCLASS_HIDDEN);
+            });
+        }
+
+        // Add callbacks for work pages
+        let lastScrollPos = 0;
+
+        let workpageExitButtons = [].slice.call(document.getElementsByClassName(STR.CCLASS_WORKPAGE_EXITBUTTON));
+        for (let index = 0; index < workpageExitButtons.length; index++) {
+            let workpage = workpageExitButtons[index].parentElement;
+            workpage.addEventListener("click", function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            workpageExitButtons[index].addEventListener("click", function (e) {
+                e.preventDefault();
+                e.stopPropagation();
+               workpage.classList.add(STR.CCLASS_WORKPAGE_HIDDEN); 
+               document.body.style.position = "unset";
+               document.documentElement.scroll({
+                   top: lastScrollPos,
+                   left: 0,
+                   behavior: "auto"
+               });
+               toggleScrolling(true);
+               document.documentElement.style.scrollBehavior = "smooth";
+            });
+        }
+
+
+        let workpageTiles = [].slice.call(document.getElementsByClassName(STR.CCLASS_WORKPAGE_TILE));
+        for (let index = 0; index < workpageTiles.length; index++) {
+            let workpage = workpageTiles[index].children[1];
+            if (workpage) {
+                workpageTiles[index].addEventListener("click", function (e) {
+                    workpage.classList.remove(STR.CCLASS_WORKPAGE_HIDDEN);
+                    document.documentElement.style.scrollBehavior = "auto";
+                    lastScrollPos = document.documentElement.scrollTop;
+                    document.body.style.position = "fixed";
+
+                    toggleScrolling(false);
+                });
+            }
         }
     });
 })(document);
